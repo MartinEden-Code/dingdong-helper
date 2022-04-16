f139acdf33335b708b7872c6a5f66752c08382c4
洪星义 <hongxingyi@yuantutech.com>
2022-04-16 08:15:55 +0800

    通过charles加入一些UserConfig内容配置

--- src/main/java/ApplicationTest.java
+++ src/main/java/ApplicationTest.java
@@ -1,3 +1,5 @@
+import org.junit.Before;
+
 import java.util.Map;
 
 /**
@@ -5,27 +7,43 @@ import java.util.Map;
  */
 public class ApplicationTest {
 
-    public static void main(String[] args) {
+    /**
+     * 配置配送信息（可以通过抓包charles抓包工具，配合pc端微信叮咚买菜小程序总获取相关配置信息，具体看Readme.md的操作步骤）
+     */
+    @Before
+    public void configAddress(){
         if (UserConfig.addressId.length() == 0) {
             System.err.println("请先执行UserConfig获取配送地址id");
             return;
         }
+    }
 
-        // 此为单次执行模式  用于在非高峰期测试下单  也必须满足3个前提条件  1.有收货地址  2.购物车有商品 3.能选择配送信息
+    public static void main(String[] args) {
+
+        Map<String, Object> cartMaps = Api.getCart(false);
+        assert cartMaps != null;
+        System.out.println(cartMaps.toString());
+
+        /*// 此为单次执行模式  用于在非高峰期测试下单  也必须满足3个前提条件  1.有收货地址  2.购物车有商品 3.能选择配送信息
+        //购物车全选
         Api.allCheck();
+        //获取购物车内容
         Map<String, Object> cartMap = Api.getCart(false);
         if (cartMap == null) {
             return;
         }
+        //获取配送信息
         Map<String, Object> multiReserveTimeMap = Api.getMultiReserveTime(UserConfig.addressId, cartMap);
         if (multiReserveTimeMap == null) {
             return;
         }
+        //获取订单确认信息
         Map<String, Object> checkOrderMap = Api.getCheckOrder(UserConfig.addressId, cartMap, multiReserveTimeMap);
         if (checkOrderMap == null) {
             return;
         }
-        Api.addNewOrder(UserConfig.addressId, cartMap, multiReserveTimeMap, checkOrderMap);
+        //提交订单
+        Api.addNewOrder(UserConfig.addressId, cartMap, multiReserveTimeMap, checkOrderMap);*/
     }
 }
 
--- src/main/java/UserConfig.java
+++ src/main/java/UserConfig.java
@@ -8,7 +8,7 @@ import java.util.Map;
 public class UserConfig {
 
     //收货地址id
-    public static final String addressId = "";
+    public static final String addressId = "6258cd6afaf704000155b004";
 
     /**
      * 提前获取收货地址id 填写到addressId上 规则为该站点可送达的默认收货地址  如果没有请自行去APP中设置好再运行
@@ -25,19 +25,19 @@ public class UserConfig {
      */
     public static Map<String, String> getHeaders() {
         Map<String, String> headers = new HashMap<>();
-        headers.put("ddmc-city-number", "");
-        headers.put("ddmc-build-version", "");
-        headers.put("ddmc-device-id", "");
-        headers.put("ddmc-station-id", "");//这个是站点id 请仔细检查和确认这个参数 进入小程序之后首页左上角是不是你所在的站点 如果不是先选择好站点再抓包 不要把站点搞错了 否则不能下单
+        headers.put("ddmc-city-number", "0901");
+        headers.put("ddmc-build-version", "2.83.0");
+        headers.put("ddmc-device-id", "osP8I0fKglD7VqVL6s2Y-AvnGKnI");
+        headers.put("ddmc-station-id", "6100c12e13d85c00011a315c");//这个是站点id 请仔细检查和确认这个参数 进入小程序之后首页左上角是不是你所在的站点 如果不是先选择好站点再抓包 不要把站点搞错了 否则不能下单
         headers.put("ddmc-channel", "applet");
         headers.put("ddmc-os-version", "[object Undefined]");
         headers.put("ddmc-app-client-id", "4");
-        headers.put("cookie", "");//用户是否登录关键信息 请仔细检查
+        headers.put("cookie", "DDXQSESSID=8e1c9179e00c1e16caadee91d9b32ab4");//用户是否登录关键信息 请仔细检查
         headers.put("ddmc-ip", "");
-        headers.put("ddmc-longitude", "");
-        headers.put("ddmc-latitude", "");
-        headers.put("ddmc-api-version", "");
-        headers.put("ddmc-uid", "");
+        headers.put("ddmc-longitude", "120.08312");
+        headers.put("ddmc-latitude", "30.313689");
+        headers.put("ddmc-api-version", "9.50.0");
+        headers.put("ddmc-uid", "6258ccae2891dd0001b3e21a");
         headers.put("user-agent", "Mozilla/5.0 (iPhone; CPU iPhone OS 15_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.18(0x1800123c) NetType/WIFI Language/zh_CN");//有反馈说PC端小程序的UserAgent不能下单 如果是PC端抓包直接使用此Agent即可
         headers.put("referer", "https://servicewechat.com/wx1e113254eda17715/422/page-frame.html");
         return headers;
@@ -50,20 +50,20 @@ public class UserConfig {
      */
     public static Map getBody() {
         Map body = new HashMap<>();
-        body.put("uid", "");
-        body.put("longitude", "");
-        body.put("latitude", "");
-        body.put("station_id", "");//这个是站点id 请仔细检查和确认这个参数 进入小程序之后首页左上角是不是你所在的站点 如果不是先选择好站点再抓包 不要把站点搞错了 否则不能下单
-        body.put("city_number", "");
-        body.put("api_version", "");
-        body.put("app_version", "");
+        body.put("uid", "6258ccae2891dd0001b3e21a");
+        body.put("longitude", "120.08312");
+        body.put("latitude", "30.313689");
+        body.put("station_id", "6100c12e13d85c00011a315c");//这个是站点id 请仔细检查和确认这个参数 进入小程序之后首页左上角是不是你所在的站点 如果不是先选择好站点再抓包 不要把站点搞错了 否则不能下单
+        body.put("city_number", "0901");
+        body.put("api_version", "9.50.0");
+        body.put("app_version", "2.83.0");
         body.put("applet_source", "");
         body.put("channel", "applet");
         body.put("app_client_id", "4");
         body.put("sharer_uid", "");
-        body.put("openid", "");
+        body.put("openid", "osP8I0fKglD7VqVL6s2Y-AvnGKnI");
         body.put("h5_source", "");
-        body.put("device_token", "");//用户是否登录关键信息 请仔细检查
+        body.put("device_token", "WHJMrwNw1k/FKPjcOOgRd+BQL2rFH1y7E29Qhygw5xnLhVw3zJn2UyuInQqhdptav/TDkRqHFf7ywH7yqBKiJHrY3d+QHmz2udCW1tldyDzmauSxIJm5Txg==1487582755342");//用户是否登录关键信息 请仔细检查
         return body;
     }
 
